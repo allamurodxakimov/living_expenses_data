b440405b7a7882f1bb94bfa665cc98501a328c35
allamurodxakimov <xakimov2806@gmail.com>
2023-10-25 16:07:01 -0700

    dictinory

--- get_data.py
+++ get_data.py
@@ -11,7 +11,9 @@ def get_data(file_path: str) -> dict:
         dict: dictionary of data
     """
     pass
-
+    file_path=open("data.json").read()
+    ls=json.loads(file_path)
+    print(ls)
 
 # test
 file_path = "data.json"
