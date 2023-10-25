eb248a6521f18990d553fb33d0ba7b83acb30c61
allamurodxakimov <xakimov2806@gmail.com>
2023-10-25 16:08:26 -0700

    max value

--- most_expensive.py
+++ most_expensive.py
@@ -10,7 +10,13 @@ def most_expensive(file_path: str) -> str:
     Returns:
         str: most expensive item
     """
-    pass
+    file_path=open("data.json").read()
+    ls=json.loads(file_path)
+    ma=ls["rent"]
+    for i in ls:
+        if ma<ls[i]:
+            ma=ls[i]
+    return ma
 
 # test
 file_path = "data.json"
